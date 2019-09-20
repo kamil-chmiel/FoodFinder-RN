ac92175ab5e0cff6cea706cec23de12e375bd8fa
Kamil Chmiel <kamil.chmiel@ready4s.com>
2019-09-20 15:43:15 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import SearchScreen from './src/screens/SearchScreen'
+
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Food Finder'
+  }
+})
+
+export default createAppContainer(navigator)
